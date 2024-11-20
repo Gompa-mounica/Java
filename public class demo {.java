8463a57281288c792338713776cda0edcb368551
Gompa-mounica <141259819+Gompa-mounica@users.noreply.github.com>
2024-11-20 18:54:53 +0530

    demo of even odd

---
+++ public class demo {.java
@@ -0,0 +1,15 @@
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=5;
+				if(num%2==0)
+				{
+					System.out.println(num+"is even:");
+				}
+				else {
+					System.out.println(num+"is odd:");
+				}
+	}
+
+}
